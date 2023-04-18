356886ab0e5404f92a3372b5b11dfdc680211fdd
joesang <joe.chang.1996@hotmail.com>
2023-04-18 18:24:55 -0400

    Add Css

--- js_components/createuser.js
+++ js_components/createuser.js
@@ -1,35 +1,35 @@
 //Funcion para Crear el usuario
-const openModalButton = document.getElementById("add_user");
-const modal = document.getElementById('modal_add');
+const openModalButton = document.getElementById('open-modal-add');
+const modal = document.getElementById('modal-add');
 const cancelButton = document.getElementById('cancel-button');
 
 openModalButton.addEventListener('click', () => {
-    modal.style.display = 'block';
-    modal.appendChild(closeModalButton);
+  modal.style.display = 'block';
+  modal.appendChild(closeModalButton);
 });
 
 cancelButton.addEventListener('click', () => {
-    const nameInput = document.getElementById('name');
-    const emailInput = document.getElementById('email');
-    nameInput.value = '';
-    emailInput.value = '';
-    modal.style.display = 'none'; // cerrar el modal
+  const nameInput = document.getElementById('name');
+  const emailInput = document.getElementById('email');
+  nameInput.value = '';
+  emailInput.value = '';
+  modal.style.display = 'none'; // cerrar el modal
 });
 
 
-const addUserForm = document.getElementById("add_user_form");
+const addUserForm = document.getElementById("add-user-form");
 addUserForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    const nameInput = document.getElementById("name");
-    const emailInput = document.getElementById("email");
-    const name = nameInput.value;
-    const email = emailInput.value;
+  event.preventDefault();
+  const nameInput = document.getElementById("name");
+  const emailInput = document.getElementById("email");
+  const name = nameInput.value;
+  const email = emailInput.value;
 
-    fetch("http://localhost:4000/graphql", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            query: `mutation {
+  fetch("http://localhost:4000/graphql", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      query: `mutation {
     createUser(name: "${name}", email: "${email}") {
       id
       name
@@ -38,14 +38,14 @@ addUserForm.addEventListener("submit", function (event) {
       updatedAt
     }
   }`,
-        }),
+    }),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      nameInput.value = "";
+      emailInput.value = "";
+      modal.style.display = 'none';
     })
-        .then((res) => res.json())
-        .then((res) => {
-            console.log(res);
-            nameInput.value = "";
-            emailInput.value = "";
-            modal.style.display = 'none';
-        })
-        .catch((err) => console.error(err));
+    .catch((err) => console.error(err));
 });
